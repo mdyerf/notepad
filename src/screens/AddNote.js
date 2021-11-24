809103ae2d16746fadc517e11ec67f2d0de3b047
mdyerf <mdyerf1380@gmail.com>
2021-11-24 14:24:24 +0330

    add a new note feature

--- src/screens/AddNote.js
+++ src/screens/AddNote.js
@@ -1,15 +1,105 @@
-import { Button } from "@material-ui/core";
-import React from "react";
+import { Button, Snackbar, TextField } from "@material-ui/core";
+import { ChevronLeft, NoteAdd } from "@material-ui/icons";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import routes from "../constants/routes";
+import { useSelector } from "react-redux";
+import { addNote, editNote, getNoteById } from "../store/notes";
+import Alert from "@material-ui/lab/Alert";
 
 function AddNote(props) {
+  const { id } = useParams();
+
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
+  const [toastOpen, setToastOpen] = useState(false);
+  const [error, setError] = useState(false);
+
+  const note = useSelector(getNoteById(id));
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (id) {
+      setTitle(note.title);
+      setText(note.text);
+    }
+  }, [id, note]);
+
   return (
     <>
-      <div>Adding a new note</div>
       <Link to={routes.Home}>
-        <Button>Back</Button>
+        <Button
+          style={{ margin: 20 }}
+          variant="contained"
+          color="secondary"
+          startIcon={<ChevronLeft />}
+        >
+          Back
+        </Button>
       </Link>
+      <form
+        className="form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (title.trim() === "" || text.trim() === "") {
+            setToastOpen(false);
+            setError(true);
+            return;
+          }
+          if (id) dispatch(editNote({ ...note, title, text }));
+          else dispatch(addNote({ title, text }));
+          setText("");
+          setTitle("");
+          setError(false);
+          setToastOpen(true);
+        }}
+      >
+        <TextField
+          style={{ margin: 10, width: "50%" }}
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          label="title"
+          variant="outlined"
+        />
+        <TextField
+          style={{ margin: 10, width: "100%" }}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          label="text"
+          variant="outlined"
+        />
+        <Button
+          style={{ width: "fit-content", margin: 10 }}
+          type="submit"
+          variant="contained"
+          color="secondary"
+          startIcon={<NoteAdd />}
+        >
+          Submit
+        </Button>
+      </form>
+      <Snackbar open={toastOpen} autoHideDuration={3000}>
+        <Alert
+          onClose={() => setToastOpen(false)}
+          severity="success"
+          variant="filled"
+          sx={{ width: "100%" }}
+        >
+          Note Added
+        </Alert>
+      </Snackbar>
+      <Snackbar open={error} autoHideDuration={3000}>
+        <Alert
+          onClose={() => setError(false)}
+          severity="error"
+          variant="filled"
+          sx={{ width: "100%" }}
+        >
+          Please fill all inputs
+        </Alert>
+      </Snackbar>
     </>
   );
 }
