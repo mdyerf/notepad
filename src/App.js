da78d2ce77aae8dbbe106b2ae23df08e03fc28a0
mdyerf <mdyerf1380@gmail.com>
2021-11-27 11:44:03 +0330

    extracting app layout

--- src/App.js
+++ src/App.js
@@ -3,16 +3,15 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import routes from "./constants/routes";
 import AddNote from "./screens/AddNote";
 import Notes from "./screens/Notes";
-import SideBar from "./components/SideBar";
 import { Provider } from "react-redux";
 import store from "./store/store";
+import AppLayout from "./containers/AppLayout";
 
 function App() {
   
   return (
     <Provider store={store}>
-      <SideBar />
-      <div style={{ marginTop: 63 }}>
+      <AppLayout>
         <BrowserRouter>
           <Routes>
             <Route exact path={routes.Home} element={<Notes />} />
@@ -20,7 +19,7 @@ function App() {
             <Route path={`${routes.AddNote}/:id`} element={<AddNote />} />
           </Routes>
         </BrowserRouter>
-      </div>
+      </AppLayout>
     </Provider>
   );
 }
